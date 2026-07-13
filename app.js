cc9956a2f831b8bd0242dd70eaf8cf896edac45f
Julczan <julek111222h@gmail.com>
2026-07-13 11:49:46 +0200

    link the styles file

--- app.js
+++ app.js
@@ -10,8 +10,12 @@ require("dotenv/config");
 require("./config/passport");
 
 const app = express();
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
 
 app.use(
   expressSession({
